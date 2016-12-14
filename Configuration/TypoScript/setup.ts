41be5be9a967e8ac141f9df89bcd8ebdddea3caa
Joe Schäfer <schaefer@scw.info>
2016-12-14 17:05:23 +0100

    Initial commit

---
+++ Configuration/TypoScript/setup.ts
@@ -0,0 +1,20 @@
+page = PAGE
+page {
+  10 = FLUIDTEMPLATE
+  10 {
+      templateName = Default
+      templateRootPaths {
+          10 = EXT:tgfit/Resources/Private/Templates
+      }
+      layoutRootPaths {
+          10 = EXT:tgfit/Resources/Private/Layouts
+      }
+      partialRootPaths {
+          10 = EXT:tgfit/Resources/Private/Partials
+      }
+  }
+
+  includeCSS {
+    styles = EXT:tgfit/Resources/Public/Styles.scss
+  }
+}
