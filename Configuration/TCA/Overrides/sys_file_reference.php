bcc1673dc44106f3be0eabb3bfc33996fe3d0f12
Joe Schäfer <schaefer@scw.info>
2017-05-04 09:40:05 +0200

    Image ratios für Bühne

---
+++ Configuration/TCA/Overrides/sys_file_reference.php
@@ -0,0 +1,29 @@
+<?php
+
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
+/*
+    Defaults
+
+    'ratios' => array(
+        '1.7777777777777777' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.16_9',
+        '1.3333333333333333' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.4_3',
+        '1' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.1_1',
+        'NaN' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.free',
+    ),
+
+*/
+
+$GLOBALS['TCA']['sys_file_reference']['columns']['crop']['config'] = array(
+    'type' => 'imageManipulation',
+    'ratios' => array(
+        '1.7777777777777777' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.16_9',
+        '1.3333333333333333' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.4_3',
+        '1' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.1_1',
+        'NaN' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.free',
+        '2.4083333333' => 'Stage / Bühne neben Titel',
+        '3.6099585062' => 'Stage / Bühne OHNE Titel'
+    ),
+);
