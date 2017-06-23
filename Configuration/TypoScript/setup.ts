c5b20b259654febf6cab5e8682535999c478a0d7
Joe Schäfer <schaefer@scw.info>
2017-06-23 14:08:06 +0200

    add favicons

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -24,9 +24,9 @@ lib.content.get {
 }
 
 /**
-* Base and setup
-*
-*/
+ * Base and setup
+ *
+ */
 config.baseURL = http://{$tgfit.domain}/
 #config.tx_frontend_editing = 1
 
@@ -54,9 +54,38 @@ page {
 }
 
 /**
-* Styles and scripts
+* Favicons
 *
 */
+
+page.headerData {
+    301 = COA
+    301.1 = TEXT
+    301.1.value (
+<link rel="apple-touch-icon" sizes="57x57" href="typo3conf/ext/tgfit/Resources/Public/Favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="typo3conf/ext/tgfit/Resources/Public/Favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="typo3conf/ext/tgfit/Resources/Public/Favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="typo3conf/ext/tgfit/Resources/Public/Favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="typo3conf/ext/tgfit/Resources/Public/Favicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="typo3conf/ext/tgfit/Resources/Public/Favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="typo3conf/ext/tgfit/Resources/Public/Favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="typo3conf/ext/tgfit/Resources/Public/Favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="typo3conf/ext/tgfit/Resources/Public/Favicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="typo3conf/ext/tgfit/Resources/Public/Favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="typo3conf/ext/tgfit/Resources/Public/Favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="typo3conf/ext/tgfit/Resources/Public/Favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="typo3conf/ext/tgfit/Resources/Public/Favicon/favicon-16x16.png">
+<link rel="manifest" href="typo3conf/ext/tgfit/Resources/Public/Favicon/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="typo3conf/ext/tgfit/Resources/Public/Favicon/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+    )
+}
+
+/**
+ * Styles and scripts
+ *
+ */
 page {
     includeCSS {
         vendor = EXT:tgfit/Resources/Public/Css/vendor.css
